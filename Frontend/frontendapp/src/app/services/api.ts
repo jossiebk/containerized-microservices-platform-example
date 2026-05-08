71c095fe2a5453a581625a46431fc33db86b7641
Jossie Bismarck Castrillo Fajardo <bkcastrillo@gmail.com>
2026-05-08 16:53:19 -0600

    Update - docker compose completo

--- Frontend/frontendapp/src/app/services/api.ts
+++ Frontend/frontendapp/src/app/services/api.ts
@@ -15,6 +15,15 @@ export class ApiService {
   paymentsUrl = 'http://localhost:5007/api/payments';
   reviewsUrl = 'http://localhost:5009/api/reviews';
 
+  // usersUrl = 'http://users-api:5001/api/users';
+  // productsUrl = 'http://products-api:5002/api/products';
+  // shippingUrl = 'http://shipping-api:5003/api/shipping';
+  // inventoryUrl = 'http://inventory-api:5004/api/inventory';
+  // cartUrl = 'http://cart-api:5005/api/cart';
+  // ordersUrl = 'http://orders-api:5006/api/orders';
+  // paymentsUrl = 'http://payments-api:5007/api/payments';
+  // reviewsUrl = 'http://reviews-api:5009/api/reviews';
+
   get(url: string) {
     return this.http.get(url);
   }
